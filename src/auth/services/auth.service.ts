4211b2715839bf8a060fe748ee7a469834a3e14a
Connor Burge <connor.d.burge@gmail.com>
2025-07-16 17:52:27 -0400

    wip

--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -24,7 +24,7 @@ export class AuthService implements IAuthService {
     private readonly sessionService: ISessionService,
     private readonly configService: ConfigService,
     private readonly jwtService: JwtService,
-  ) { }
+  ) {}
 
   async verifyUser(email: string, password: string): Promise<User> {
     try {
@@ -79,17 +79,17 @@ export class AuthService implements IAuthService {
     const expirationAccessToken = new Date();
     expirationAccessToken.setSeconds(
       expirationAccessToken.getSeconds() +
-      parseInt(
-        this.configService.getOrThrow<string>('JWT_ACCESS_EXPIRATION'),
-      ),
+        parseInt(
+          this.configService.getOrThrow<string>('JWT_ACCESS_EXPIRATION'),
+        ),
     );
 
     const expirationRefreshToken = new Date();
     expirationRefreshToken.setSeconds(
       expirationRefreshToken.getSeconds() +
-      parseInt(
-        this.configService.getOrThrow<string>('JWT_REFRESH_EXPIRATION'),
-      ),
+        parseInt(
+          this.configService.getOrThrow<string>('JWT_REFRESH_EXPIRATION'),
+        ),
     );
 
     const payload: ITokenPayload = {
